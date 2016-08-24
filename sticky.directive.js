d72c978d542b914b71fb2d4873a840fc3ba65300
vask3n <vask3n@gmail.com>
2016-08-24 11:21:36 +0200

    space reservation, performance improvemend & media queries
    - performance improvement
    - added media from (bootstrap-sizes)
    - added dummy element to keep space reserved

--- sticky.directive.js
+++ sticky.directive.js
@@ -10,6 +10,8 @@
     function controller($element, $window) {
 
       var self = this;
+      self.isStickyActive = false;
+
       var stickyFrom = isStickyFrom();
       var stickyUntil = isStickyUntil();
       var stickyOffset = self.stickyOffset || 0;
@@ -22,27 +24,29 @@
         if (angular.isDefined(self.stickyFromMedia)) {
           return $window.innerWidth > mediaQueries[self.stickyFromMedia];
         }
-        return true
+        return true;
       }
 
       function sticky(el){
-        // Freeze width/height/fixed/top.
-        el.css({
-          height: el[0].offsetHeight + 'px',
-          width: el[0].offsetWidth + 'px',
-          position: 'fixed',
-          top: stickyOffset + 'px'
-        });
+        if (!self.isStickyActive) {
+          el.css({
+            height: el[0].offsetHeight + 'px',
+            width: el[0].offsetWidth + 'px',
+            position: 'fixed',
+            top: stickyOffset + 'px'
+          });
+          dummySwitch();
+        };
+        self.isStickyActive = true;
       }
 
       function unsticky(el){
         // Reset all values.
-        el.css({
-          height:'',
-          width:'',
-          position:'',
-          top:''
-        });
+        if (self.isStickyActive) {
+          el.css({ height: '', width: '', position: '', top: '' });
+          dummySwitch();
+        };
+        self.isStickyActive = false;
       }
 
       function isStickyFrom() {
@@ -58,7 +62,18 @@
         if (angular.isDefined(self.stickyUntil)) {
           return document.querySelector(self.stickyUntil);
         }
-        return undefined;
+        return null;
+      }
+
+      function dummySwitch() {
+        // Reserve space so layout won't fail
+        if (!self.isStickyActive) {
+          var dummy = document.createElement('div');
+          angular.element(dummy).css({ height: $element[0].offsetHeight + 'px', width: $element[0].offsetWidth + 'px' });
+          $element.parent()[0].insertBefore(dummy, $element[0]);
+        } else {
+          $element[0].previousElementSibling.remove();
+        }
       }
 
       angular.element($window).on('scroll', function(){
